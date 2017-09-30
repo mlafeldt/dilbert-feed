446c6ba88b3646d50fec04ef8c70e9f5a6993ed3
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2017-09-30 18:43:50 +0200

    Serve via HTTP

--- main.go
+++ main.go
@@ -4,11 +4,59 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+const atomTemplate = `<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+ <title>Dilbert</title>
+ {{ range . }}
+ <entry>
+   <title>{{ .Title }}</title>
+   <link href="{{ .Strip }}"/>
+   <updated>{{ .Date }}</updated>
+   <content type="html"><p><img src="{{ .Image }}" title="{{ .Title }}"></p></content>
+ </entry>
+ {{ end }}
+</feed>
+`
+
+func main() {
+	port := "3000"
+	if v := os.Getenv("PORT"); v != "" {
+		port = v
+	}
+	log.Printf("Listening on port %s", port)
+
+	http.HandleFunc("/atom.xml", handler)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var comics []Comic
+	now := time.Now()
+
+	for i := 1; i <= 30; i++ {
+		t := now.AddDate(0, 0, -i)
+		date := fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
+		comic, err := comicForDate(date)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%+v\n", comic)
+		comics = append(comics, *comic)
+	}
+
+	t, err := template.New("feed").Parse(atomTemplate)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, comics)
+}
 
 type Comic struct {
 	Date  string
@@ -50,39 +98,4 @@ func comicForDate(date string) (*Comic, error) {
 		Image: image,
 		Strip: strip,
 	}, nil
-}
-
-const atomTemplate = `<?xml version="1.0" encoding="utf-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom">
- <title>Dilbert</title>
- {{ range . }}
- <entry>
-   <title>{{ .Title }}</title>
-   <link href="{{ .Strip }}"/>
-   <updated>{{ .Date }}</updated>
-   <content type="html"><p><img src="{{ .Image }}" title="{{ .Title }}"></p></content>
- </entry>
- {{ end }}
-</feed>
-`
-
-func main() {
-	var comics []Comic
-	now := time.Now()
-
-	for i := 1; i <= 30; i++ {
-		t := now.AddDate(0, 0, -i)
-		date := fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
-		comic, err := comicForDate(date)
-		if err != nil {
-			log.Fatal(err)
-		}
-		comics = append(comics, *comic)
-	}
-
-	t, err := template.New("feed").Parse(atomTemplate)
-	if err != nil {
-		log.Fatal(err)
-	}
-	t.Execute(os.Stdout, comics)
 }
