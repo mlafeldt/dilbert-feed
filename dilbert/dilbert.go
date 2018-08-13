73f77ba6a997ec88692bc9621a43a06b4504fccb
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-08-13 23:04:00 +0200

    Give download more time to succeed

--- dilbert/dilbert.go
+++ dilbert/dilbert.go
@@ -23,7 +23,7 @@ func ComicForDate(date string) (*Comic, error) {
 		return nil, err
 	}
 
-	client := &http.Client{Timeout: 5 * time.Second}
+	client := &http.Client{Timeout: 15 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
