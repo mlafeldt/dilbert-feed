58dae637d81b27c56766596f748e6caccff7727e
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-08-15 12:59:29 +0200

    Split up logic into two Lambda functions

---
+++ download-strip/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+
+	"github.com/mlafeldt/dilbert-feed/dilbert"
+)
+
+type Input struct {
+	Date string `json:"date"`
+}
+
+type Output struct {
+	*dilbert.Comic
+	UploadURL string `json:"upload_url"`
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(input Input) (*Output, error) {
+	var date string
+
+	if input.Date != "" {
+		date = strings.TrimSpace(input.Date)
+		if len(date) != 10 {
+			return nil, fmt.Errorf("input date %q has invalid length", date)
+		}
+		if len(strings.Split(date, "-")) != 3 {
+			return nil, fmt.Errorf("input date %q has invalid format", date)
+		}
+	}
+
+	comic, err := dilbert.NewComic(date)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("DEBUG: %+v", comic)
+
+	bucket := os.Getenv("BUCKET_NAME")
+
+	log.Printf("INFO: Uploading strip %s to bucket %q ...", comic.StripURL, bucket)
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Get(comic.ImageURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, err
+	}
+
+	uploadResult, err := s3manager.NewUploader(sess).Upload(&s3manager.UploadInput{
+		Bucket:      aws.String(bucket),
+		Key:         aws.String(fmt.Sprintf("strips/%s.gif", comic.Date)),
+		Body:        resp.Body,
+		ContentType: aws.String("image/gif"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("INFO: Upload completed: %s", uploadResult.Location)
+
+	return &Output{comic, uploadResult.Location}, nil
+}
--- generate-feed/main.go
+++ generate-feed/main.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"strings"
 	"text/template"
 	"time"
 
@@ -42,13 +40,10 @@ const feedTemplate = `<rss version="2.0">
 </rss>
 `
 
-type Input struct {
-	Date string `json:"date"`
-}
+type Input struct{}
 
 type Output struct {
-	*dilbert.Comic
-	UploadURL string `json:"upload_url"`
+	FeedURL string `json:"feed_url"`
 }
 
 func main() {
@@ -56,57 +51,12 @@ func main() {
 }
 
 func handler(input Input) (*Output, error) {
-	var date string
-
-	if input.Date != "" {
-		date = strings.TrimSpace(input.Date)
-		if len(date) != 10 {
-			return nil, fmt.Errorf("input date %q has invalid length", date)
-		}
-		if len(strings.Split(date, "-")) != 3 {
-			return nil, fmt.Errorf("input date %q has invalid format", date)
-		}
-	}
-
-	comic, err := dilbert.NewComic(date)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Printf("DEBUG: %+v", comic)
-
-	bucket := os.Getenv("BUCKET_NAME")
-
-	log.Printf("INFO: Uploading strip %s to bucket %q ...", comic.StripURL, bucket)
-
-	client := &http.Client{Timeout: 10 * time.Second}
-	resp, err := client.Get(comic.ImageURL)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
 	sess, err := session.NewSession()
 	if err != nil {
 		return nil, err
 	}
 
-	uploadResult, err := s3manager.NewUploader(sess).Upload(&s3manager.UploadInput{
-		Bucket:      aws.String(bucket),
-		Key:         aws.String(fmt.Sprintf("strips/%s.gif", comic.Date)),
-		Body:        resp.Body,
-		ContentType: aws.String("image/gif"),
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	output := Output{comic, uploadResult.Location}
-
-	log.Printf("INFO: Upload completed: %s", output.UploadURL)
-
-	// TODO: move the following to a separate Lambda function
-
+	bucket := os.Getenv("BUCKET_NAME")
 	bucketLocation, err := s3.New(sess).GetBucketLocation(&s3.GetBucketLocationInput{
 		Bucket: aws.String(bucket),
 	})
@@ -119,8 +69,8 @@ func handler(input Input) (*Output, error) {
 	now := time.Now()
 
 	for i := 0; i < feedLength; i++ {
-		t := now.AddDate(0, 0, -i)
-		date := fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
+		day := now.AddDate(0, 0, -i)
+		date := fmt.Sprintf("%d-%02d-%02d", day.Year(), day.Month(), day.Day())
 		comics = append(comics, dilbert.Comic{
 			Date:     date,
 			ImageURL: fmt.Sprintf("https://%s.s3.%s.amazonaws.com/strips/%s.gif", bucket, bucketRegion, date),
@@ -135,7 +85,7 @@ func handler(input Input) (*Output, error) {
 	var buf bytes.Buffer
 	templ.Execute(&buf, comics)
 
-	uploadResult, err = s3manager.NewUploader(sess).Upload(&s3manager.UploadInput{
+	uploadResult, err := s3manager.NewUploader(sess).Upload(&s3manager.UploadInput{
 		Bucket:      aws.String(bucket),
 		Key:         aws.String("v0/rss.xml"),
 		Body:        &buf,
@@ -147,5 +97,5 @@ func handler(input Input) (*Output, error) {
 
 	log.Printf("INFO: Upload completed: %s", uploadResult.Location)
 
-	return &output, nil
+	return &Output{uploadResult.Location}, nil
 }
