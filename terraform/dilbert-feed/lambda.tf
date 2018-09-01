227535837132c7f19c2adeb30211f1a3e37851ac
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-09-01 17:23:53 +0200

    Remove $LATEST from Lambda function ARNs

--- terraform/dilbert-feed/lambda.tf
+++ terraform/dilbert-feed/lambda.tf
@@ -1,7 +1,9 @@
 data "aws_lambda_function" "get_strip" {
   function_name = "${var.service}-${var.stage}-get-strip"
+  qualifier     = ""
 }
 
 data "aws_lambda_function" "gen_feed" {
   function_name = "${var.service}-${var.stage}-gen-feed"
+  qualifier     = ""
 }
