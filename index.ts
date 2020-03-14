f793365803f72c6477309d87638dfdafd1358000
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2020-03-14 12:07:39 +0100

    Deploy Lambdas

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
 import s3 = require('@aws-cdk/aws-s3');
 
 interface DilbertFeedStackProps extends cdk.StackProps {
@@ -21,6 +22,47 @@ export class DilbertFeedStack extends cdk.Stack {
       id: 'DeleteStripsAfter30Days',
       prefix: stripsDir,
       expiration: cdk.Duration.days(30)
+    });
+
+    const getStrip = new lambda.Function(this, 'GetStripFunc', {
+      functionName: `${id}-get-strip`,
+      code: lambda.Code.fromAsset('bin/get-strip'),
+      handler: 'handler',
+      runtime: lambda.Runtime.GO_1_X,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+        BUCKET_PREFIX: stripsDir
+      }
+    });
+    bucket.grantPut(getStrip);
+
+    const genFeed = new lambda.Function(this, 'GenFeedFunc', {
+      functionName: `${id}-gen-feed`,
+      code: lambda.Code.fromAsset('bin/gen-feed'),
+      handler: 'handler',
+      runtime: lambda.Runtime.GO_1_X,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+        BUCKET_PREFIX: stripsDir
+      }
+    });
+    bucket.grantPut(genFeed);
+
+    // @ts-ignore
+    const heartbeat = new lambda.Function(this, 'HeartbeatFunc', {
+      functionName: `${id}-heartbeat`,
+      code: lambda.Code.fromAsset('heartbeat'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        HEARTBEAT_ENDPOINT: props.heartbeatEndpoint
+      }
     });
   }
 }
