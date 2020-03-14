292179ac3ec658a8a396ba68f02f6fd8036e9ecf
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2020-03-14 11:17:13 +0100

    Add stack params

--- index.ts
+++ index.ts
@@ -1,12 +1,27 @@
 import cdk = require('@aws-cdk/core');
 
+interface DilbertFeedStackProps extends cdk.StackProps {
+  bucketName?: string;
+  heartbeatEndpoint: string;
+}
+
 export class DilbertFeedStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props: DilbertFeedStackProps) {
     super(scope, id, props);
   }
 }
 
 const app = new cdk.App();
-new DilbertFeedStack(app, 'dilbert-feed-dev-ts', { tags: { STAGE: 'dev' } });
-new DilbertFeedStack(app, 'dilbert-feed-prod-ts', { tags: { STAGE: 'prod' } });
+
+// TODO: remove -ts suffix
+new DilbertFeedStack(app, 'dilbert-feed-dev-ts', {
+  heartbeatEndpoint: 'https://hc-ping.com/33868fe9-9efc-414a-b882-a598a2b09dea',
+  tags: { STAGE: 'dev' }
+});
+new DilbertFeedStack(app, 'dilbert-feed-prod-ts', {
+  bucketName: 'dilbert-feed-ts',
+  heartbeatEndpoint: 'https://hc-ping.com/4fb7e55d-fe13-498b-bfaf-73cbf20e279e',
+  tags: { STAGE: 'prod' }
+});
+
 app.synth();
