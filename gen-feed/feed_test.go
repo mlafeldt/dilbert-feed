84082b7c493dcf333726aaa8885b2af8dcf318e1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2020-03-21 16:52:16 +0100

    gen-feed: add tests for new behavior

--- gen-feed/feed_test.go
+++ gen-feed/feed_test.go
@@ -2,21 +2,50 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager/s3manageriface"
 	"github.com/google/go-cmp/cmp"
 )
+
+type mockS3Client struct {
+	s3iface.S3API
+	Titles map[string]*string
+}
+
+func (m *mockS3Client) HeadObject(input *s3.HeadObjectInput) (*s3.HeadObjectOutput, error) {
+	return &s3.HeadObjectOutput{
+		Metadata: map[string]*string{
+			"Title": m.Titles[aws.StringValue(input.Key)],
+		},
+	}, nil
+}
 
 func TestFeedGenerator(t *testing.T) {
 	var buf bytes.Buffer
 	date, _ := time.Parse("2006-01-02", "2018-10-01")
+
+	mockClient := &mockS3Client{
+		Titles: map[string]*string{
+			"strips/2018-10-01.gif": aws.String("Use Company Products"),
+			"strips/2018-09-29.gif": aws.String("Fine Lines"),
+		},
+	}
+
 	g := FeedGenerator{
-		BucketName: "dilbert-feed-example",
+		BucketName: "dilbert-feed-test",
 		StripsDir:  "strips/",
 		StartDate:  date,
 		FeedLength: 3,
+		S3Client:   mockClient,
 	}
 
 	if err := g.Generate(&buf); err != nil {
@@ -33,7 +62,36 @@ func TestFeedGenerator(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if diff := cmp.Diff(string(want), string(got)); diff != "" {
+	if diff := cmp.Diff(strings.TrimSpace(string(want)), string(got)); diff != "" {
+		t.Error(diff)
+	}
+}
+
+type mockS3Uploader struct {
+	s3manageriface.UploaderAPI
+}
+
+func (m *mockS3Uploader) Upload(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
+	return &s3manager.UploadOutput{
+		Location: fmt.Sprintf("https://%s.s3.amazonaws.com/%s",
+			aws.StringValue(input.Bucket), aws.StringValue(input.Key)),
+	}, nil
+}
+
+func TestFeedUploader(t *testing.T) {
+	u := FeedUploader{
+		BucketName: "dilbert-feed-test",
+		FeedPath:   "some/path/feed.xml",
+		S3Uploader: &mockS3Uploader{},
+	}
+	want := "https://dilbert-feed-test.s3.amazonaws.com/some/path/feed.xml"
+
+	got, err := u.Upload(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
 		t.Error(diff)
 	}
 }
